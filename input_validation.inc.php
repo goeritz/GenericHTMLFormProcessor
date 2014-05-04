7d021e344b293c7943fc1529c19e609842d46143
Jan Vogt <news.jan.vogt@me.com>
2014-05-04 14:08:22 +0200

    Update input_validation for use of prefixed control value. Don't use implicit string constants anymore

--- input_validation.inc.php
+++ input_validation.inc.php
@@ -4,43 +4,43 @@
 1. paste contents of this file after the <?php tag in Generic HTML Form Processor 
 
 2. paste next line in HTML form that is to be validated 
-<input name="from_page" type="hidden" value="1">
+<input name="GHFPvar_from_page" type="hidden" value="1">
 
 3. delete unwanted checks on $_POST[] variables below 
 
 4. Done! The remaining steps are optional for advanced users
 
 4a. to validate input from several HTML forms, paste the next line in each HTML form that is to be validated and adjust the value of the hidden variable
-<input name="from_page" type="hidden" value="2">
+<input name="GHFPvar_from_page" type="hidden" value="2">
 
 4b. you may add your own input checks below
 
 */
 
-if ($_POST[from_page] == "1") //HTML form to be validated contains hidden variable "from_page" with value "1" 
+if ($_POST['GHFPvar_from_page'] == "1") //HTML form to be validated contains hidden variable "GHFPvar_from_page" with value "1" 
 {
-	if ($_POST[apples] < "1")   //value of input field is less than 1
+	if ($_POST['apples'] < "1")   //value of input field is less than 1
 	{
 	echo "Please tell us whether you like apples.";
 	echo "<br><br><a href=\"javascript:history.back()\">back</a>";
 	exit;
 	}
 
-	if ($_POST[peaches] == "" || $_POST[age] == "") 	//one or both input fields are empty
+	if ($_POST['peaches'] == "" || $_POST['age'] == "") 	//one or both input fields are empty
 	{
 	echo "Please tell us your age and whether you like peaches!";
 	echo "<br><br><a href=\"javascript:history.back()\">back</a>";
 	exit;
 	}
 
-	if (!is_numeric ($_POST[age]))  	//age is not numeric
+	if (!is_numeric ($_POST['age']))  	//age is not numeric
 	{
 	echo "Please indicate your age in full years using numbers only!";
 	echo "<br><br><a href=\"javascript:history.back()\">back</a>";
 	exit;
 	}
 
-	if (strlen($_POST[age]) < 2)  	//age is less than 2 characters long
+	if (strlen($_POST['age']) < 2)  	//age is less than 2 characters long
 	{
 	echo "Are you really that young?";
 	echo "<br><br><a href=\"javascript:history.back()\">back</a>";
